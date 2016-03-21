ea914b0ddb782c024a59f94979cf805c8a246ec5
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-03-21 11:30:18 -0400

    ugh

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,11 @@
 class whats::config (
-  $config_path      = $::whats::config_path,
+  $config           = $::whats::config,
   $puppetdb_server  = $::whats::puppetdb_server,
   $puppetdb_port    = $::whats::puppetdb_port,
   $search_domains   = $::whats::search_domains,
   ) {
 
-    file {$config_path:
+    file {$config:
       content => template('whats/whats.yaml.erb'),
       owner   => root,
       group   => root,
