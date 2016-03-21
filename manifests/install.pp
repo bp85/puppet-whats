abb9ec6c0ff679e66f34073cce43d4b7748f384d
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-03-21 12:20:19 -0400

    updte deps..hacky

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,10 @@ class whats::install (
   $version  = $::whats::version,
   $provider = $::whats::provider,
   ) {
-
+    $deps = ['gcc','ruby-devel']
+    package {$deps:
+      ensure => installed,
+    } ->
     package {'puppet-whats':
       ensure   => $version,
       provider => $provider,
