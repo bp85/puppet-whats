393b50a8f99c789998a3b9abe5f72fbcd27a96a7
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-03-21 12:35:26 -0400

    fix deps a bit

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,13 +2,21 @@ class whats::install (
   $version  = $::whats::version,
   $provider = $::whats::provider,
   ) {
-    $deps = ['gcc','ruby-devel']
-    package {$deps:
-      ensure => installed,
-    } ->
+
+    if ! defined(Package['gcc']) {
+      packge {'gcc':
+        ensure => installed,
+      }
+    }
+    if ! defined(Package['ruby-devel']) {
+      packge {'ruby-devel':
+        ensure => installed,
+      }
+    }
+
     package {'puppet-whats':
       ensure   => $version,
       provider => $provider,
+      require  => [Package['gcc'],Package['ruby-devel']],
     }
-
   }
